8277fb7bc767a7e5f9235e039e7284d33b4694b4
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-07-12 13:28:01 +0530

    fixed footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,17 @@ import "../App.css";
 export default function Footer() {
   return (
     <div className="stickyFooter">
-      Made with ❤️ in
-      <img
+      Made with ❤️ in{" "}
+      {/* <img
         src="https://img.icons8.com/doodle/48/000000/india.png"
         height="20px"
         width="20px"
-      />
+      /> */}
       India by{" "}
-      <a href="#" style={{ color: "yellow", textDecoration: "none" }}>
+      <a
+        href="https://www.instagram.com/theshubhagrwl/"
+        style={{ color: "yellow", textDecoration: "none" }}
+      >
         Shubh
       </a>
     </div>
