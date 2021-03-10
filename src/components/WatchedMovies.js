542d65c69cac1000fb24a5deb1b49601786d5ccd
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-03-10 10:53:42 +0530

    improved watched movie list

--- src/components/WatchedMovies.js
+++ src/components/WatchedMovies.js
@@ -1,42 +1,23 @@
 import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
 import { MovieContext } from "../MovieContext";
-
-import fakeData from "../testData";
+import { Grid, Typography } from "@material-ui/core";
+// import fakeData from "../testData";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
+    flexGrow: 1,
     marginBottom: "10vh",
-    background: "transparent",
-  },
-  gridList: {
-    flexWrap: "nowrap",
-    justifyContent: "center",
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: "translateZ(0)",
-    width: "100%",
   },
   title: {
-    color: theme.palette.primary,
-  },
-  titleBar: {
-    // background:
-    //   "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
-  },
-  gridTile: {
-    width: "100%",
+    color: "#fff",
   },
   image: {
     opacity: 0.4,
-    width: "100%",
-    height: "100%",
+    width: "15vw",
+  },
+  watchedMovieCard: {
+    background: "#3939396b",
   },
 }));
 
@@ -49,24 +30,20 @@ const WatchedMovies = () => {
       {/* {fakeData.length > 1 ? ( */}
       {/* {fakeData.map((tile) => ( */}
       {contextData.watched.length > 0 ? (
-        <GridList className={classes.gridList} cols={5} cellHeight={"auto"}>
+        <Grid container spacing={3}>
           {contextData.watched.map((tile) => (
-            <GridListTile key={tile.imdbID} className={classes.gridTile}>
+            <Grid item xs className={classes.watchedMovieCard}>
               <img
                 className={classes.image}
                 src={tile.Poster}
                 alt={tile.Title}
               />
-              <GridListTileBar
-                title={tile.Title}
-                classes={{
-                  root: classes.titleBar,
-                  title: classes.title,
-                }}
-              />
-            </GridListTile>
+              <Typography variant="subtitle1" className={classes.title}>
+                {tile.Title}
+              </Typography>
+            </Grid>
           ))}
-        </GridList>
+        </Grid>
       ) : (
         ""
       )}
