58c769f424d9f54e7dea072eb0b49e93bfa8465f
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-05-04 18:23:45 +0530

    after signin redirects to home

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -24,7 +24,7 @@ const Signin = () => {
       <Navbar />
 
       {user ? (
-        <Redirect to="/profile" />
+        <Redirect to="/" />
       ) : (
         <div
           className="flexBox"
