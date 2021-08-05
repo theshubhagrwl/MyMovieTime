b9b0239c1741604eb3c9177a0a12c472d22b7ef6
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-08-05 21:37:42 +0530

    removed buymecoffee button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "../App.css";
-import Coffee from "./BuyMeACoffeeButton";
+// import Coffee from "./BuyMeACoffeeButton";
 
 export default function Footer() {
   return (
     <div className="stickyFooter">
-      {/* Made with ❤️ in India by{" "}
+      Made with ❤️ in India by{" "}
       <a
         href="https://theshubhagrwl.in/"
         style={{ color: "yellow", textDecoration: "none" }}
       >
         Shubh{" "}
-      </a> */}
-      <Coffee />
+      </a>
+      {/* <Coffee /> */}
     </div>
   );
 }
