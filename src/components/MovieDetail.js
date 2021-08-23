ef4d46989af20673f9de3339876b5923e1fd367a
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-08-23 08:40:02 +0530

    getting details from TMDB api

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import axios from "axios";
+import Axios from "axios";
 
 import { movieDetail } from "../testData";
 import { Button, Typography } from "@material-ui/core";
 
-const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,84 +29,101 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     justifyContent: "center",
   },
+  poster: {
+    maxHeight: "50vh",
+  },
 }));
 
 const MovieDetail = (props) => {
   const { id } = props.match.params;
   const [movieData, setMovieData] = useState({});
+  const [loading, setLoading] = useState(false);
   const classes = useStyles();
 
-  const getData = async () => {
-    const response = await axios.get(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`
-    );
-    setMovieData(response.data);
-    // setMovieData(() => movieDetail);
+  const getData = () => {
+    setLoading(true);
+    Axios.get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
+    )
+      .then((res) => {
+        setMovieData(res.data);
+        console.log(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  const MovieRating = () => {
-    console.log(movieData);
-    return (
-      <div>
-        {movieData.Ratings.map((item) => (
-          <li key={item.Source}>
-            <Typography variant="h6">
-              {item.Source} : {item.Value}
-            </Typography>
-          </li>
-        ))}
-        {/* <li>
-          <Typography variant="h6">
-            IMDB: {movieData.Ratings[0].Value}
-          </Typography>
-        </li>
-        <li>
-          <Typography variant="h6">
-            {movieData.Ratings[1].Source} : {movieData.Ratings[1].Value}
-          </Typography>
-        </li> */}
-      </div>
-    );
-  };
+  const ShowTrailer = ({ data }) => (
+    <>
+      {data.videos.results.map((item) =>
+        item.site === "YouTube"
+          ? `https://www.youtube.com/watch?v=${item.key}`
+          : ""
+      )}
+    </>
+  );
 
   return (
     <div className={classes.root}>
-      <div className={classes.leftContainer}>
-        <img alt={movieData.Title} src={movieData.Poster} />
-        <div className={classes.buttonGroup}>
-          <Button
-            size="small"
-            variant="contained"
-            color="primary"
-            style={{ marginBottom: "15px" }}
-            // onClick={() => {
-            //   setMTime(item);
-            // }}
-          >
-            Mark Watched
-          </Button>
-        </div>
-      </div>
-      <div className={classes.rightContainer}>
-        <ul className={classes.listView}>
-          <Typography variant="h3">{movieData.Title}</Typography>
-          <Typography variant="subtitle1" gutterBottom>
-            {movieData.Year}
-          </Typography>
-          <li>{movieData.Genre}</li>
-          <li>{movieData.Director}</li>
-          <br />
-          <Typography variant="h6">Plot</Typography>
-          <li>{movieData.Plot}</li>
-          <br />
-
-          {/* <MovieRating /> */}
-        </ul>
-      </div>
+      {loading ? (
+        <Typography variant="h2">Loading</Typography>
+      ) : (
+        <>
+          <div className={classes.leftContainer}>
+            <img
+              alt={movieData.title}
+              className={classes.poster}
+              src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+            />
+            <div className={classes.buttonGroup}>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                style={{ marginBottom: "15px" }}
+                // onClick={() => {
+                //   setMTime(item);
+                // }}
+              >
+                Mark Watched
+              </Button>
+            </div>
+          </div>
+          <div className={classes.rightContainer}>
+            <ul className={classes.listView}>
+              <Typography variant="h3">{movieData.title}</Typography>
+              <Typography variant="subtitle1" gutterBottom>
+                {movieData.release_date}
+              </Typography>
+              {movieData.genres ? (
+                <div>
+                  {movieData.genres.map((item) => (
+                    <span key={item.id}>
+                      <Typography variant="body2">{item.name}</Typography>
+                    </span>
+                  ))}
+                </div>
+              ) : (
+                ""
+              )}
+              <br />
+              <Typography variant="h6">Overview</Typography>
+              <li>{movieData.overview}</li>
+              <br />
+              <div>
+                {movieData.videos ? <ShowTrailer data={movieData} /> : ""}
+              </div>
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 };
